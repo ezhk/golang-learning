d1a9359de6f22a107f934cb48c141f06f01d6006
Andrey Kiselev <ezhik@ezhik.info>
2020-06-21 19:41:51 +0300

    LRU logic released

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -3,20 +3,68 @@ package hw04_lru_cache //nolint:golint,stylecheck
 type Key string
 
 type Cache interface {
-	// Place your code here
+	Set(key string, value interface{}) bool
+	Get(key string) (interface{}, bool)
+	Clear()
 }
 
 type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	capacity int
+	Queue    List
+	Items    map[string]*listItem
 }
 
 type cacheItem struct {
-	// Place your code here
+	cKey   string
+	cValue interface{}
 }
 
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	list := NewList()
+	return &lruCache{
+		capacity: capacity,
+		Queue:    list,
+		Items:    make(map[string]*listItem),
+	}
+}
+
+func (c lruCache) Set(key string, value interface{}) bool {
+	if item, ok := c.Items[key]; ok {
+		ci := item.Value.(cacheItem)
+		ci.cValue = value
+		item.Value = ci
+
+		c.Queue.MoveToFront(item)
+		return true
+	}
+
+	for c.Queue.Len() >= c.capacity {
+		latestItem := c.Queue.Back()
+
+		c.Queue.Remove(latestItem)
+		delete(c.Items, latestItem.Value.(cacheItem).cKey)
+	}
+
+	item := cacheItem{
+		cKey:   key,
+		cValue: value,
+	}
+	c.Items[key] = c.Queue.PushFront(item)
+	return false
+}
+
+func (c lruCache) Get(key string) (interface{}, bool) {
+	if item, ok := c.Items[key]; ok {
+		c.Queue.MoveToFront(item)
+		return (item.Value).(cacheItem).cValue, true
+	}
+
+	return nil, false
+}
+
+func (c lruCache) Clear() {
+	for c.Queue.Len() > 0 {
+		c.Queue.Remove(c.Queue.Back())
+	}
+	c.Items = make(map[string]*listItem)
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,7 +50,53 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(1)
+
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Equal(t, nil, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+	})
+
+	t.Run("push logic", func(t *testing.T) {
+		c := NewCache(3)
+
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+
+		// make aaa and bbb more frequent
+		c.Get("aaa")
+		c.Get("bbb")
+
+		c.Set("ddd", 400)
+
+		// check purged value
+		val, ok := c.Get("ccc")
+		require.False(t, ok)
+		require.Equal(t, nil, val)
+
+		// check exist values
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
 	})
 }
 
@@ -64,14 +110,14 @@ func TestCacheMultithreading(t *testing.T) {
 	go func() {
 		defer wg.Done()
 		for i := 0; i < 1_000_000; i++ {
-			c.Set(Key(strconv.Itoa(i)), i)
+			c.Set(strconv.Itoa(i), i)
 		}
 	}()
 
 	go func() {
 		defer wg.Done()
 		for i := 0; i < 1_000_000; i++ {
-			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+			c.Get(strconv.Itoa(rand.Intn(1_000_000)))
 		}
 	}()
 
