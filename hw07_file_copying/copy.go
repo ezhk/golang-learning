bc6c1bf2099365ecddc4e3e95f46c79bf0f5a483
Andrey Kiselev <ezhik@ezhik.info>
2020-08-03 22:01:51 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,93 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
+	ErrFileNotExist          = errors.New("file doesn't exist")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	err := fileErrors(fromPath, offset)
+	if err != nil {
+		return err
+	}
+
+	// use limited reader instead default reader
+	limit = changeZeroLimit(fromPath, limit)
+	limitedReader, err := defineLimitedReader(fromPath, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	// define bar reader
+	bar := pb.Full.Start64(limit)
+	defer bar.Finish()
+	barReader := bar.NewProxyReader(limitedReader)
+
+	// define default writer
+	writer, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, barReader)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func fileErrors(fromPath string, offset int64) error {
+	fileSt, err := os.Stat(fromPath)
+	if os.IsNotExist(err) {
+		return ErrFileNotExist
+	}
+	// check other errors
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileSt.Size()
+	fmt.Println(fileSize)
+	if fileSize < 0 {
+		return ErrUnsupportedFile
+	}
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	return nil
+}
+
+func changeZeroLimit(fromPath string, limit int64) int64 {
+	fileSt, _ := os.Stat(fromPath)
+
+	if limit < 1 {
+		limit = fileSt.Size()
+	}
+
+	return limit
+}
+
+func defineLimitedReader(fromPath string, offset, limit int64) (io.Reader, error) {
+	reader, err := os.Open(fromPath)
+	if err != nil {
+		return reader, err
+	}
+	_, err = reader.Seek(offset, io.SeekStart)
+	if err != nil {
+		return reader, err
+	}
+
+	limitedReader := io.LimitReader(reader, limit)
+	return limitedReader, nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,20 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	t.Run("Check not exists file", func(t *testing.T) {
+		err := Copy("not-exist-file", "", 0, 0)
+		require.Equal(t, err, ErrFileNotExist)
+	})
+
+	t.Run("Too long offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "", int64(math.Exp2(20)), 0)
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
 }
