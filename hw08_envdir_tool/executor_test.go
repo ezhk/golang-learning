855dae1b2c2f186204fdcdf1fa8e58be3e610509
Andrey Kiselev <ezhik@ezhik.info>
2020-08-15 12:55:43 +0300

    HW8 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("not exist file", func(t *testing.T) {
+		exitCode := RunCmd([]string{"not-exist-command"}, nil)
+		require.EqualValues(t, 1, exitCode)
+	})
+
+	t.Run("check output", func(t *testing.T) {
+		r, w, err := os.Pipe()
+		require.Nil(t, err)
+
+		os.Stdout = w
+		exitCode := RunCmd([]string{"echo", "-n", "test"}, nil)
+		w.Close()
+
+		require.EqualValues(t, 0, exitCode)
+		out, err := ioutil.ReadAll(r)
+		require.Nil(t, err)
+
+		require.Equal(t, "test", string(out))
+	})
 }
