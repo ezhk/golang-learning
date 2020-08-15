39fc99629396df2f3ea4f888a18c44e11e7ac0cd
Andrey Kiselev <ezhik@ezhik.info>
2020-08-15 13:02:50 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,47 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadFileValue(t *testing.T) {
+	t.Run("not exist file", func(t *testing.T) {
+		_, err := readFileValue("testdata/env/not-exist")
+		require.True(t, os.IsNotExist(err))
+	})
+
+	t.Run("empty file", func(t *testing.T) {
+		val, err := readFileValue("testdata/env/UNSET")
+		require.Nil(t, err)
+		require.Equal(t, "", val)
+	})
+
+	t.Run("heavy variable", func(t *testing.T) {
+		sample := fmt.Sprintf("   foo\nwith new line")
+
+		val, err := readFileValue("testdata/env/FOO")
+		require.Nil(t, err)
+		require.Equal(t, sample, val)
+	})
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("not exist dir", func(t *testing.T) {
+		_, err := ReadDir("testdata/env-not-exist")
+		err, ok := err.(*os.PathError)
+
+		require.True(t, ok)
+		require.NotNil(t, err)
+	})
+
+	t.Run("complex parse check", func(t *testing.T) {
+		envMap, err := ReadDir("testdata/env")
+		require.Nil(t, err)
+
+		require.Equal(t, envMap, Environment{"UNSET": "", "BAR": "bar", "HELLO": "\"hello\"", "FOO": "   foo\nwith new line"})
+	})
 }
