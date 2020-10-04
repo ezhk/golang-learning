e6c05cf77a7d8408396e2ffb6fb1c5bd2cc65119
Andrey Kiselev <ezhik@ezhik.info>
2020-10-04 20:39:38 +0300

    tools has beed added

---
+++ hw12_13_14_15_calendar/tools.go
@@ -0,0 +1,11 @@
+// +build tools
+
+package main
+
+import (
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/pressly/goose/cmd/goose"
+)
